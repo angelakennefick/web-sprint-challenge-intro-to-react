e5618f95328829ad6d465602abd093a90dc060b2
angelakennefick <78941432+angelakennefick@users.noreply.github.com>
2021-04-16 18:04:19 -0500

    Character displayed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
+import Character from "../src/components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,77 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [query, setQuery] = useState("")
+  const [characters, setCharacters] = useState([]);
+  const [search, setSearch] = useState("");
+
+   const SWAPI = `https://swapi.dev/api/people/?search=${query}`;
+  // const [characterId, setCharacterId] = useState("1");
+
+  // const openDetails = id => {
+  //   setCharacterId(id);
+  // }
+
+  // const closeDetails = () => {
+  //   setCharacterId(null);
+  // }
+
+  useEffect(() => {
+    axios.get(SWAPI)
+    .then(res => {
+      setCharacters(res.data);
+    })
+    .catch(err => {
+      console.log("oops", err);
+    })
+    console.log("SWAPI fetched...");
+  }, [/*query*/]);
+
+  // const updateSearch = e => {
+  //   setSearch(e.target.value)
+  // };
+
+  // const getSearch = e => {
+  //   e.preventDefault();
+  //   setQuery(search);
+  //   setSearch("");
+  // };
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {/* <form 
+      onSubmit= {getSearch} 
+      className= "search-form"> */}
+        {/* <input 
+        className= "search-bar" 
+        type= "text" 
+        value= {search} 
+        onChange= {updateSearch}
+        />
+        <button className= "search-button" 
+        type= "submit">
+          Use The Force...
+        </button>
+      </form> */}
+      {characters.map(character =>(
+        <Character 
+          key= {character.name}
+          name= {character.name} 
+          height= {character.height} 
+          mass= {character.mass}
+          hair_color= {character.hair_color}
+          skin_color= {character.skin_color}
+          eye_color= {character.eye_color}
+          birth_year= {character.birth_year}
+          gender= {character.gender}
+          // homeworld= {character.homeworld}
+          films= {character.films}
+          // species= {character.species}
+          // vehicles={character.vehicles}
+          // starships={character.starships}
+        />
+      ))}
     </div>
   );
 }
